7bbb0039ece3a11a03ae09683a5ee7cb77e1a814
K-HANBI <khb9812@naver.com>
2020-10-09 17:45:43 +0900

    한비123123123

--- Home_fit/src/Ggg.java
+++ Home_fit/src/Ggg.java
@@ -3,6 +3,6 @@ public class Ggg {
 	public static void main(String[] args) {
 		System.out.println("진짜");
 		System.out.println("될거야!");
-		System.out.println("한비");
+		System.out.println("한비123");
 	}
 }
