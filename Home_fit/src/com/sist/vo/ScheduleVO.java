430be84f6e70e6a6a19aa046ac87c2f3e8e6b852
Chihoon <achoon91@gmail.com>
2020-10-16 17:20:54 +0900

    안치훈 10-16 coach 추가

---
+++ Home_fit/src/com/sist/vo/ScheduleVO.java
@@ -0,0 +1,50 @@
+package com.sist.vo;
+
+
+public class ScheduleVO {
+	private int coach_no;
+	private String month;
+	private String time;
+	private String place;
+	private int schedule_no;
+	private String reserve_check;
+	public int getCoach_no() {
+		return coach_no;
+	}
+	public void setCoach_no(int coach_no) {
+		this.coach_no = coach_no;
+	}
+	public String getMonth() {
+		return month;
+	}
+	public void setMonth(String month) {
+		this.month = month;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public String getPlace() {
+		return place;
+	}
+	public void setPlace(String place) {
+		this.place = place;
+	}
+	public int getSchedule_no() {
+		return schedule_no;
+	}
+	public void setSchedule_no(int schedule_no) {
+		this.schedule_no = schedule_no;
+	}
+	public String getReserve_check() {
+		return reserve_check;
+	}
+	public void setReserve_check(String reserve_check) {
+		this.reserve_check = reserve_check;
+	}
+
+	
+}
+	
